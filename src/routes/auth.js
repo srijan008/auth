7db36f083a9471aef97eff3a0f98010ea78e7ae2
srijan008 <srijankumar008@gmail.com>
2024-10-11 15:47:53 +0530

    Add baseUrl

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -21,7 +21,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:8000/auth/google/callback"
+        callbackURL: `${process.env.BASE_URL}/auth/google/callback`
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
